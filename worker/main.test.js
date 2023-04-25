71c40342b1bfc9d796725dd683447cdd2acb2922
Alessandro Menezes <alessandroasm@gmail.com>
2023-04-25 11:33:30 -0400

    Add with negatives (worker)

--- worker/main.test.js
+++ worker/main.test.js
@@ -3,3 +3,7 @@ const { add } = require("./main");
 it("Should add correctly", () => {
   expect(add(3, 4)).toBe(7);
 });
+
+it("Should add a negative number correctly", () => {
+  expect(add(4, -3)).toBe(1);
+});
